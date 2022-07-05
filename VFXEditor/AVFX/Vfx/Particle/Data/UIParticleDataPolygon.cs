d46813dfba7fb39e4997537b4b41e77323a7f48e
ocealot <kaminsky.michael@outlook.com>
2022-07-05 12:56:52 -0400

    more case-sensitive nonsense

--- VFXEditor/AVFX/Vfx/Particle/Data/UIParticleDataPolygon.cs
+++
@@ -1,9 +0,0 @@
-using VFXEditor.AVFXLib.Particle;
-
-namespace VFXEditor.AVFX.VFX {
-    public class UIParticleDataPolygon : UIData {
-        public UIParticleDataPolygon( AVFXParticleDataPolygon data ) {
-            Tabs.Add( new UICurve( data.Count, "Count" ) );
-        }
-    }
-}
