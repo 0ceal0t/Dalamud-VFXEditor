f813da1983237a747784c26f3f4781a1b95914f7
ocealot <kaminsky.michael@outlook.com>
2024-04-20 00:53:50 -0400

    Update MtrlDyeTableRow.cs

--- VFXEditor/Formats/MtrlFormat/Table/Dye/MtrlDyeTableRow.cs
+++ VFXEditor/Formats/MtrlFormat/Table/Dye/MtrlDyeTableRow.cs
@@ -17,27 +17,42 @@ namespace VfxEditor.Formats.MtrlFormat.Table.Dye {
     public partial class MtrlDyeTableRow {
         public readonly MtrlTables Tables;
         public readonly ParsedShort Template = new( "Template" );
-        public readonly ParsedFlag<DyeRowFlags> Flags = new( "Flags", 2 );
+        public readonly ParsedFlag<DyeRowFlags> Flags = new( "Flags" );
 
         public MtrlDyeTableRow( MtrlTables tables ) {
             Tables = tables;
         }
 
         public void Read( BinaryReader reader ) {
-            Dalamud.Log( $"{reader.BaseStream.Position:X4}" );
+            if( !Tables.Extended ) {
+                var value = reader.ReadUInt16();
+                Flags.Value = ( DyeRowFlags )( object )( value & 0b11111 );
+                Template.Value = value >> 5;
+            }
+            else {
+                var value = reader.ReadUInt32();
+                Flags.Value = ( DyeRowFlags )( object )( int )( value & 0b11111 );
+                Template.Value = ( int )( value >> 16 );
+            }
 
-            Flags.Read( reader );
-            Template.Value = Flags.IntValue >> 5;
+            // TODO: still some work
 
-            if( Tables.Extended ) reader.ReadUInt16(); // temp
-            // 1001100
+            // 322B // old (16)
+            // 11001000101011
+            // 00110010001 | 01011
+
+            // 0x191000B // new (32)
+            // 1100100010000000000001011
+            // 110010001 | 00000000000 | 01011
         }
 
         public void Write( BinaryWriter writer ) {
-            var value = Flags.IntValue;
-            writer.Write( ( ushort )( value & 0x1F | Template.Value << 5 ) );
-
-            if( Tables.Extended ) FileUtils.Pad( writer, 2 ); // temp
+            if( !Tables.Extended ) {
+                writer.Write( ( ushort )( ( Flags.IntValue & 0b11111 ) | ( Template.Value << 5 ) ) );
+            }
+            else {
+                writer.Write( ( uint )( ( Flags.IntValue & 0b11111 ) | ( Template.Value << 16 ) ) );
+            }
         }
 
         public void Draw() {
