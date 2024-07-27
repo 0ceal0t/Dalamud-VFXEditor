be00131b93b3c6dd4014a4f27c2661093daf3a85
ocealot <kaminsky.michael@outlook.com>
2024-07-27 19:21:49 -0400

    fix npc atch list

--- VFXEditor/Select/Formats/AtchSelectDialog.cs
+++ VFXEditor/Select/Formats/AtchSelectDialog.cs
@@ -1,11 +1,13 @@
 using VfxEditor.Formats.AtchFormat;
 using VfxEditor.Select.Tabs.Character;
+using VfxEditor.Select.Tabs.Npc;
 
 namespace VfxEditor.Select.Formats {
     public class AtchSelectDialog : SelectDialog {
         public AtchSelectDialog( string id, AtchManager manager, bool isSourceDialog ) : base( id, "atch", manager, isSourceDialog ) {
             GameTabs.AddRange( [
-                new CharacterTabAtch( this, "Character" )
+                new NpcTabAtch( this, "Npc" ),
+                new CharacterTabAtch( this, "Character" ),
             ] );
         }
     }
---
+++ VFXEditor/Select/Tabs/Npc/NpcTabAtch.cs
@@ -0,0 +1,44 @@
+using Lumina.Excel.GeneratedSheets2;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VfxEditor.Select.Tabs.Npc {
+    public class NpcTabAtch : SelectTab<NpcRow> {
+        public NpcTabAtch( SelectDialog dialog, string name ) : base( dialog, name, "Npc-Atch" ) { }
+
+        public override void LoadData() {
+            var file = Dalamud.DataManager.GetFile( "chara/xls/attachoffset/attachoffsetexist.waoe" );
+            using var ms = new MemoryStream( file.Data );
+            using var reader = new BinaryReader( ms );
+
+            var count = reader.ReadUInt16();
+            var offsetsExist = new HashSet<ushort>();
+            for( var i = 0; i < count; i++ ) offsetsExist.Add( reader.ReadUInt16() );
+
+            // ================
+
+            var nameToString = NpcTab.NameToString;
+
+            var baseToName = JsonConvert.DeserializeObject<Dictionary<string, uint>>( File.ReadAllText( SelectDataUtils.BnpcPath ) );
+            var battleNpcSheet = Dalamud.DataManager.GetExcelSheet<BNpcBase>();
+            foreach( var entry in baseToName ) {
+                if( !nameToString.TryGetValue( entry.Value, out var name ) ) continue;
+
+                var bnpcRow = battleNpcSheet.GetRow( uint.Parse( entry.Key ) );
+                if( !NpcTab.BnpcValid( bnpcRow ) ) continue;
+
+                var modelChara = bnpcRow.ModelChara.Value;
+                var id = ( ushort )( modelChara.Type == 2 ? modelChara.Model + 10000 : modelChara.Model );
+                if( !offsetsExist.Contains( id ) ) continue;
+
+                Items.Add( new NpcRow( modelChara, name ) );
+            }
+
+        }
+
+        protected override void DrawSelected() {
+            Dialog.DrawPaths( Selected.AtchPath, Selected.Name, SelectResultType.GameNpc );
+        }
+    }
+}
