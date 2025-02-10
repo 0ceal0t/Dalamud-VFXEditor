34a06b479863c0b3b83ad322c441565f31bfba46
ocealot <kaminsky.michael@outlook.com>
2025-02-10 18:47:41 -0500

    Update C120.cs

--- VFXEditor/Formats/TmbFormat/Entries/C120.cs
+++ VFXEditor/Formats/TmbFormat/Entries/C120.cs
@@ -5,25 +5,25 @@ using VfxEditor.TmbFormat.Utils;
 namespace VfxEditor.TmbFormat.Entries {
     public class C120 : TmbEntry {
         public const string MAGIC = "C120";
-        public const string DISPLAY_NAME = "";
+        public const string DISPLAY_NAME = "Controller Vibration";
         public override string DisplayName => DISPLAY_NAME;
         public override string Magic => MAGIC;
 
         public override int Size => 0x18;
         public override int ExtraSize => 0;
 
-        private readonly ParsedInt Unk1 = new( "Unknown 1", value: 1 );
+        private readonly ParsedInt Duration = new( "Duration", value: 1 );
         private readonly ParsedInt Unk2 = new( "Unknown 2" );
-        private readonly ParsedInt Unk3 = new( "Unknown 3", value: 100 );
+        private readonly ParsedInt WaveType = new( "Wave Type", value: 100 );
 
         public C120( TmbFile file ) : base( file ) { }
 
         public C120( TmbFile file, TmbReader reader ) : base( file, reader ) { }
 
         protected override List<ParsedBase> GetParsed() => [
-            Unk1,
+            Duration,
             Unk2,
-            Unk3
+            WaveType
         ];
     }
 }
