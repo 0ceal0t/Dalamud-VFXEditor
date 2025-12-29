b44c41f6521d91f602b07fe960399dbd9abb806d
EchoDidAThing <154573455+EchoDidAThing@users.noreply.github.com>
2025-12-29 05:25:02 -0500

    Minor fix to clicking into the ID entry of markers on an SCD removing the last character and not allowing you to enter 4;

--- VFXEditor/Formats/ScdFormat/Music/Marker/ScdAudioMarker.cs
+++ VFXEditor/Formats/ScdFormat/Music/Marker/ScdAudioMarker.cs
@@ -8,12 +8,14 @@ using VfxEditor.Parsing.String;
 using VfxEditor.ScdFormat;
 using VfxEditor.Ui.Components;
 using VfxEditor.Utils;
+using Serilog;
+using Dalamud.Plugin.Services;
 
 namespace VfxEditor.Formats.ScdFormat.Music.Marker {
     public class ScdAudioMarker {
         private ScdAudioEntry Entry;
 
-        public readonly ParsedPaddedString Id = new( "Id", 4, 0x00 );
+        public readonly ParsedPaddedString Id = new( "Id", 5, 0x00 );
         public readonly ParsedFloat LoopStart = new( "Loop Start" );
         public readonly ParsedFloat LoopEnd = new( "Loop End" );
 
@@ -36,8 +38,9 @@ namespace VfxEditor.Formats.ScdFormat.Music.Marker {
         public void Read( BinaryReader reader ) {
             Id.Value = FileUtils.ReadString( reader, 4 );
             reader.ReadUInt32();
-            LoopStart.Value = ( float )reader.ReadInt32() / Entry.SampleRate;
-            LoopEnd.Value = ( float )reader.ReadInt32() / Entry.SampleRate;
+            LoopStart.Value = reader.ReadSingle() / ( float )Entry.SampleRate;
+            Dalamud.Log("Input Loopstart:" + LoopStart.Value.ToString());
+            LoopEnd.Value = reader.ReadSingle() / ( float )Entry.SampleRate;
             var numMarkers = reader.ReadInt32();
 
             for( var i = 0; i < numMarkers; i++ ) {
@@ -53,8 +56,9 @@ namespace VfxEditor.Formats.ScdFormat.Music.Marker {
         public void Write( BinaryWriter writer ) {
             FileUtils.WriteString( writer, Id.Value );
             writer.Write( GetSize() );
-            writer.Write( ( int )( LoopStart.Value * Entry.SampleRate ) );
-            writer.Write( ( int )( LoopEnd.Value * Entry.SampleRate ) );
+            writer.Write( LoopStart.Value * ( float )Entry.SampleRate );
+            Dalamud.Log( "Output Loopstart:" + (( LoopStart.Value * Entry.SampleRate )).ToString() );
+            writer.Write( LoopEnd.Value * ( float )Entry.SampleRate );
             writer.Write( Markers.Count );
             foreach( var marker in Markers ) writer.Write( ( int )Math.Round( marker.Value * Entry.SampleRate, MidpointRounding.AwayFromZero ) );
 
